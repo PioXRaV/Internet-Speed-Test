ad3918378977a74206747885d19b41c2f4864d1d
Ayberk Arif Kesikbaş <70178220+PioXRaV@users.noreply.github.com>
2023-08-27 14:54:07 +0300

    Add files via upload

--- Internet Speed Test.py
+++ Internet Speed Test.py
@@ -1,14 +1,23 @@
+import socket
 import speedtest
 
-test = speedtest.Speedtest()
-
-print("Internet Speed ​​Testing")
-
-print("\nDownload Speed Testing")
-ds = test.download() / 1048576
-print("Upload Speed Testing")
-us = test.upload() / 1048576
-
-ping = round(test.results.ping)
-
-print(f"\nPing: {ping} ms\nDownload: {ds:.2f} Mbps, Upload: {us:.2f} Mbps")
+try:
+    
+    socket.create_connection(("www.google.com", 80))
+    
+    test = speedtest.Speedtest()
+    
+    print("Internet Speed ​​Testing, Please Wait.")
+    
+    print("\nDownload Speed Testing")
+    ds = test.download() / 1048576
+    print("Upload Speed Testing")
+    us = test.upload() / 1048576
+    
+    ping = round(test.results.ping)
+    
+    print(f"\nPing: {ping} ms\nDownload: {ds:.2f} Mbps, Upload: {us:.2f} Mbps")
+    
+except OSError:
+        
+    print("No Internet Connection")
